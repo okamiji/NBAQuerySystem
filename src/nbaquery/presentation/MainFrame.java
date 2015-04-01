c1be474d2371701c5baf843b68cf2e2bd58fb928
XNYu <yyhh0000@163.com>
2015-04-01 16:47:59 +0800

    .

--- src/nbaquery/presentation/MainFrame.java
+++ src/nbaquery/presentation/MainFrame.java
@@ -4,9 +4,11 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.FocusListener;
@@ -32,6 +34,7 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.plaf.FontUIResource;
 
+
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.util.Enumeration;
@@ -39,27 +42,29 @@ import java.util.Enumeration;
 
 public class MainFrame extends JFrame{
 
-	JPanel mainPanel,containPanel,shadowPanel;
-	JButton playerButton,teamButton;
+	JPanel mainPanel,containPanel,shadowPanel,systemPanel,sPanel;
+	JButton playerButton,teamButton,exitButton,vanishButton;
 	JTextField searchField ;
 	
 	//LogicService lcs;
 	private JTextField textField;
-	boolean isTeam=true;
+	boolean isTeam=false;
+	int LocationX;
+	int LocationY;
 	PlayerService ps;
 	TeamService ts;
 	int xOld,yOld;
 	Color background=new Color(33,122,197);
-	Color white = new Color(230,230,230);
-	Font font=new Font("造字工房尚黑G0v1细体", Font.PLAIN, 18);
-	/**
-	 * Create the application.
-	 */
-	/*public MainFrame(LogicService lcs) {
-		initialize();
-		this.lcs = lcs;
-	}*/
+	Color white = new Color(245,245,245);
+	Color gray = new Color(41,48,62);
+	Color blue = new Color(16,133,253);
+	Font font=new Font("造字工房尚黑G0v1细体", Font.PLAIN, 16);
+	Toolkit toolkit=Toolkit.getDefaultToolkit();
+    Dimension screen=toolkit.getScreenSize();
 	
+	/*
+	 * 设置字体的方法
+	 */
 	public static void setFon(Font fnt){
 		FontUIResource fontRes = new FontUIResource(fnt);
 		Enumeration<Object> keys;
@@ -72,66 +77,123 @@ public class MainFrame extends JFrame{
 		}	
 	
 	public MainFrame(PlayerService pls,TeamService tms){
+		UIManager.put("ComboBox.background", new Color(0,0,0,0));
 		this.ps=pls;
 		this.ts=tms;
-		this.setBounds(100, 100, 1000, 700);
+		Color back = new Color(0,0,0,0.0f);
+		Color white = new Color(245,245,245);
+		Color gray = new Color(41,48,62);
+		Color blue = new Color(16,133,253);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("NBAData");
-		this.setUndecorated(true);
-		mainPanel = new JPanel() {  
+		this.setSize(1118,618);
+		this.setResizable(false);
+		this.setTitle("NBAQS");
+		
+		//下面为居中
+        this.LocationX=(screen.width -this.getWidth())/2;
+        this.LocationY=(screen.height -this.getHeight())/2;       
+        this.setLocation(this.LocationX,this.LocationY);
+	    //居中完毕
+        this.setLayout(null);
+        this.setUndecorated(true);
+        this.setOpacity(0.92f);//透明度
+        this.setBackground(back);
+        
+        mainPanel = new JPanel();
+        mainPanel.setSize(200, 580);
+        mainPanel.setLocation(10,20);
+        mainPanel.setLayout(null);
+        mainPanel.setBackground(gray);
+        mainPanel.setOpaque(true);
+        
+        sPanel = new JPanel(){  
             @Override  
             protected void paintComponent(Graphics g) {  
-            	ImageIcon icon = new ImageIcon("C:/Users/小南/Desktop/大作业UI/background.png");
+            	ImageIcon icon = new ImageIcon("C:/Users/小南/Desktop/大作业UI/shadow.png");
                 Image img = icon.getImage();  
                 g.drawImage(img, 0, 0, icon.getIconWidth(), icon.getIconHeight(), icon.getImageObserver());  
             }
         };
-        mainPanel.setSize(1000, 700);
-        mainPanel.setLocation(0,0);
-        mainPanel.setLayout(null);
-        mainPanel.setBackground(white);
+        sPanel.setSize(1118, 618);
+        sPanel.setLocation(0,0);
+        sPanel.setLayout(null);
+        sPanel.setOpaque(true);
         
+
         shadowPanel = new JPanel() {  
             @Override  
             protected void paintComponent(Graphics g) {  
-            	ImageIcon icon = new ImageIcon("C:/Users/小南/Desktop/大作业UI/矩形3.png");
+            	ImageIcon icon = new ImageIcon("C:/Users/小南/Desktop/大作业UI/7.png");
                 Image img = icon.getImage();  
                 g.drawImage(img, 0, 0, icon.getIconWidth(), icon.getIconHeight(), icon.getImageObserver());  
             }
         };
-        shadowPanel.setSize(1000,700);
-        shadowPanel.setLocation(0,0);
+        shadowPanel.setSize(850, 600);
+        shadowPanel.setLocation(210,10);
         shadowPanel.setLayout(null);
         shadowPanel.setBackground(white);
+
+        systemPanel = new JPanel();
+        systemPanel.setSize(50, 580);
+        systemPanel.setLocation(1060,20);
+        systemPanel.setLayout(null);
+        systemPanel.setBackground(gray);
         
-		
-		teamButton = new JButton("\u7403\u961F\u6570\u636E",new ImageIcon("C:/Users/小南/Desktop/大作业UI/a.png"));
+		teamButton = new JButton("\u7403\u961F\u6570\u636E",new ImageIcon("C:/Users/小南/Desktop/大作业UI/球队未选中.png"));
 		teamButton.setBounds(0, 130, 202, 150);
 		teamButton.setFocusPainted(false);
 		teamButton.setBorderPainted(false);
 		teamButton.setContentAreaFilled(false);
+		teamButton.addMouseListener(new TeamMouseListener());
 		teamButton.addActionListener(new TeamListener());
-		this.getContentPane().add(teamButton);
+		mainPanel.add(teamButton);
 		
-		playerButton= new JButton("\u7403\u5458\u6570\u636E",new ImageIcon("C:/Users/小南/Desktop/大作业UI/球员按钮发光.png"));
+		playerButton= new JButton("\u7403\u5458\u6570\u636E",new ImageIcon("C:/Users/小南/Desktop/大作业UI/球员未选中.png"));
 		playerButton.setBounds(0, 280, 202, 150);
 		playerButton.addActionListener(new PlayerListener());
 		playerButton.setFocusPainted(false);
 		playerButton.setBorderPainted(false);
 		playerButton.setContentAreaFilled(false);
-		playerButton.addMouseListener(new mouseInListener());
-		this.getContentPane().add(playerButton);
+		playerButton.addMouseListener(new PlayerMouseListener());
+		mainPanel.add(playerButton);
 		
-		JButton button_1 = new JButton("\u5176\u4ED6\u529F\u80FD",new ImageIcon("C:/Users/小南/Desktop/大作业UI/热点普通.png"));
+	 	exitButton = new JButton("", new ImageIcon("C:/Users/小南/Desktop/大作业UI/close.png"));
+		exitButton.setForeground(gray);
+		exitButton.setBackground(gray);
+		exitButton.setSize(30,30);
+		exitButton.setLocation(10, 25);
+		exitButton.setBorderPainted(false);
+		exitButton.setFocusPainted(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.addActionListener(new exitButtonListener());
+		exitButton.addMouseListener(new exitListener());
+		systemPanel.add(exitButton);
+		
+		vanishButton = new JButton("", new ImageIcon("C:/Users/小南/Desktop/大作业UI/vanish.png"));
+		vanishButton.setForeground(gray);
+		vanishButton.setBackground(gray);
+		vanishButton.setSize(30,30);
+		vanishButton.setLocation(10, 65);
+		vanishButton.setBorderPainted(false);
+		vanishButton.setFocusPainted(false);
+		vanishButton.setContentAreaFilled(false);
+		vanishButton.addActionListener(new vanishButtonListener());
+		vanishButton.addMouseListener(new vanishListener());
+		systemPanel.add(vanishButton);	
+		
+		/*JButton button_1 = new JButton("\u5176\u4ED6\u529F\u80FD",new ImageIcon("C:/Users/小南/Desktop/大作业UI/热点普通.png"));
 		button_1.setBounds(0, 430, 202, 150);
 		button_1.setFocusPainted(false);
 		button_1.setBorderPainted(false);
 		button_1.setContentAreaFilled(false);
 		this.getContentPane().add(button_1);
 		button_1.addActionListener(new exitButtonListener());
-		
-		
+		*/
+		this.add(shadowPanel);
 		this.add(mainPanel);
+		this.add(systemPanel);
+		this.add(sPanel);
+		this.setVisible(true);
 		//-------------可拖动-------------
 		this.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) {
@@ -148,10 +210,7 @@ public class MainFrame extends JFrame{
 			MainFrame.this.setLocation(xx, yy);
 		}
 		});
-		
 		setFon(font);
-
-
 	}
 	
 	
@@ -163,16 +222,13 @@ public class MainFrame extends JFrame{
 		}
 		
 		public void newPlayerPanel(){
-			mainPanel.removeAll();
+			shadowPanel.removeAll();
 			PlayerTablePanel d=new PlayerTablePanel(ps);
-			d.setBounds(200,130,800,450);
-			d.setBackground(white);
-			//d.setOpaque(false);
-			mainPanel.add(d);
-			mainPanel.paintComponents(mainPanel.getGraphics());
-			mainPanel.repaint();
-			mainPanel.add(teamButton);
-			mainPanel.add(playerButton);
+			d.setBounds(0,0,800,650);
+			d.setOpaque(false);
+			shadowPanel.add(d);
+			shadowPanel.paintComponents(shadowPanel.getGraphics());
+			shadowPanel.repaint();
 			isTeam=false;
 		}
 	}
@@ -185,39 +241,43 @@ public class MainFrame extends JFrame{
 		}
 		
 		public void newTeamPanel(){
-			mainPanel.removeAll();
+			shadowPanel.removeAll();
 			TeamTablePanel d=new TeamTablePanel(ts);
-			d.setBounds(200,130,800,450);
-			d.setBackground(white);
-			//d.setOpaque(false);
-			mainPanel.add(d);
-			mainPanel.paintComponents(mainPanel.getGraphics());
-			mainPanel.repaint();
-			mainPanel.add(playerButton);
-			mainPanel.add(teamButton);
+			d.setSize(800,650);
+			d.setBackground(background);
+			d.setOpaque(false);
+			shadowPanel.add(d);
+			shadowPanel.repaint();
+			shadowPanel.revalidate();
+			//teamButton.setIcon(new ImageIcon("C:/Users/小南/Desktop/大作业UI/球队选中.png"));
+			//teamButton.repaint();
 			isTeam=true;
 		}
 		
 	}
 	
-	class mouseInListener implements MouseListener{
+	class PlayerMouseListener implements MouseListener{
 
 		@Override
 		public void mouseClicked(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-			
+			teamButton.setIcon(new ImageIcon("C:/Users/小南/Desktop/大作业UI/球队未选中.png"));
 		}
 
 		@Override
 		public void mouseEntered(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-			
+			playerButton.setIcon(new ImageIcon("C:/Users/小南/Desktop/大作业UI/球员选中.png"));
+			playerButton.repaint();
 		}
 
 		@Override
 		public void mouseExited(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-			
+			if(!isTeam)
+				return;
+			playerButton.setIcon(new ImageIcon("C:/Users/小南/Desktop/大作业UI/球员未选中.png"));
+			playerButton.repaint();
 		}
 
 		@Override
@@ -233,6 +293,126 @@ public class MainFrame extends JFrame{
 		}
 		
 	}
+	
+	class vanishButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			setExtendedState(JFrame.ICONIFIED);
+		}
+	}
+	
+	class exitListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			exitButton.setIcon(new ImageIcon("C:/Users/小南/Desktop/大作业UI/close2.png"));
+			exitButton.repaint();
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+			exitButton.setIcon(new ImageIcon("C:/Users/小南/Desktop/大作业UI/close.png"));
+			exitButton.repaint();
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+	}
+	
+	class vanishListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			vanishButton.setIcon(new ImageIcon("C:/Users/小南/Desktop/大作业UI/vanish2.png"));
+			vanishButton.repaint();
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			vanishButton.setIcon(new ImageIcon("C:/Users/小南/Desktop/大作业UI/vanish.png"));
+			vanishButton.repaint();
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+	}
+	
+	class TeamMouseListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			playerButton.setIcon(new ImageIcon("C:/Users/小南/Desktop/大作业UI/球员未选中.png"));
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			teamButton.setIcon(new ImageIcon("C:/Users/小南/Desktop/大作业UI/球队选中.png"));
+			teamButton.repaint();
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			if(isTeam)
+				return;
+			teamButton.setIcon(new ImageIcon("C:/Users/小南/Desktop/大作业UI/球队未选中.png"));
+			teamButton.repaint();
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+	}
+	
 
 	class exitButtonListener implements ActionListener {
 
--- src/nbaquery/presentation/PlayerTablePanel.java
+++ src/nbaquery/presentation/PlayerTablePanel.java
@@ -1,6 +1,7 @@
 package nbaquery.presentation;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
@@ -22,17 +23,22 @@ import javax.imageio.ImageIO;
 import javax.swing.GroupLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.UIManager;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
+import javax.swing.plaf.basic.BasicComboBoxUI;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableModel;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -62,31 +68,55 @@ public class PlayerTablePanel  extends JPanel implements TableModelListener {
 	boolean type=false;
 	JLabel playerLabel,lblName,lblTeam;
 	JScrollPane scrollPane;
+	Color white = new Color(245,245,245);
+	Color background=new Color(33,122,197);
 	
 	public PlayerTablePanel(final PlayerService ps){
-		Color background=new Color(33,122,197);
+	
 		this.ps = ps;
-		setSize(900,640);
-		tableModel=new PlayerTableModel();
-		table=new JTable(tableModel);
-		table.getModel().addTableModelListener(this);
+		setSize(800,570);
 		setLayout(null);
-		
 		setVisible(true);
+		tableModel=new PlayerTableModel();
+		table=new JTable(tableModel){ 
+			// 设置jtable的单元格为透明的
+			   public Component prepareRenderer(TableCellRenderer renderer,
+					     int row, int column) {
+					    Component c = super.prepareRenderer(renderer, row, column);
+					    if (c instanceof JComponent) {
+					     ((JComponent) c).setOpaque(false);
+					    }
+					    return c;
+					   }
+					  };
+					  
+		table.getModel().addTableModelListener(this);
+		
 		table.setFillsViewportHeight(true);  
 		table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 		
 		scrollPane = new JScrollPane(table);
-		scrollPane.setBounds(14, 53, 600, 390);
+		scrollPane.setBounds(14, 53, 600, 500);
 		
-		add(scrollPane);
+		DefaultTableCellRenderer render = new DefaultTableCellRenderer();
+		render.setOpaque(false);
+		table.setDefaultRenderer(Object.class, render);
+		//设置显示范围
+		Dimension viewSize = new Dimension();
+		viewSize.width = table.getColumnModel().getTotalColumnWidth();;
+		viewSize.height = 10*table.getRowHeight();
+		table.setPreferredScrollableViewportSize(viewSize);
+		//将JScrollPane设置为透明
+		scrollPane.getViewport().setOpaque(false);  //jScrollPanel 为table存放的容器，一般在Swing创    //  建表格时，它自动生成，原代码为：jScrollPane1 = new javax.swing.JScrollPane();
+		scrollPane.setOpaque(false);     //将中间的viewport设置为透明
+		scrollPane.setViewportView(table); //装载表格 
+		
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		int columncount = this.table.getColumnCount();
         for (int i = 0; i < columncount; i++) {
             this.table.getColumnModel().getColumn(i).setPreferredWidth(95);
         }
        
-        
         table.getTableHeader().setReorderingAllowed(false); 
         table.setColumnSelectionAllowed (false);  
         table.setRowSelectionAllowed (true);  
@@ -126,81 +156,103 @@ public class PlayerTablePanel  extends JPanel implements TableModelListener {
         table.repaint();        
 		
 		JPanel searchPanel = new JPanel();
-		searchPanel.setBounds(14, 13, 600, 40);
-		searchPanel.setOpaque(false);//透明
+		searchPanel.setBounds(14, 13, 844,40);
+		//searchPanel.setOpaque(false);//透明
 		add(searchPanel);
 		searchPanel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("\u4F4D\u7F6E");
-		lblNewLabel.setBounds(150, 3, 30, 18);
+		lblNewLabel.setBounds(169, 3, 71, 18);
 		searchPanel.add(lblNewLabel);
 		
-			positionBox = new JComboBox<String>();
-			positionBox.setBounds(194, 0, 71, 24);
-			positionBox.setBackground(background);
-			searchPanel.add(positionBox);
-			
-					JLabel label_1 = new JLabel("\u8054\u76DF");
-					label_1.setBounds(279, 3, 30, 18);
-					searchPanel.add(label_1);
+		
+		positionBox = new JComboBox<String>();
+		positionBox.setBounds(218, 0, 100, 24);
+		 positionBox.setOpaque(false);
+	        positionBox.setUI(new BasicComboBoxUI() {
+	            public void installUI(JComponent comboBox) {
+	                super.installUI(positionBox);
+	                listBox.setForeground(Color.WHITE);
+	                listBox.setSelectionBackground(new Color(0,0,0,0));
+	                listBox.setSelectionForeground(Color.BLACK);
+	            }
+	             
+	            /**
+	             * 该方法返回右边的按钮
+	             */
+	            protected JButton createArrowButton() {
+	                return super.createArrowButton();
+	            }
+	        });
+	        searchPanel.add(positionBox);
+		
+		
+		
+		
+		JLabel label_1 = new JLabel("\u8054\u76DF");
+		label_1.setBounds(279, 3, 30, 18);
+		searchPanel.add(label_1);
 					
-					leagueBox = new JComboBox<String>();
-					leagueBox.setBounds(323, 0, 71, 24);
-					searchPanel.add(leagueBox);
+		leagueBox = new JComboBox<String>();
+		leagueBox.setBounds(332, 0, 100, 24);
+		searchPanel.add(leagueBox);
 					
-					typeBox = new JComboBox<String>();
-					typeBox.setBounds(452, 0, 71, 24);
-					searchPanel.add(typeBox);
+		typeBox = new JComboBox<String>();
+		typeBox.setBounds(523, 0, 100, 24);
+		searchPanel.add(typeBox);
 					
-					JLabel label_2 = new JLabel("\u7C7B\u578B");
-					label_2.setBounds(408, 3, 30, 18);
-					searchPanel.add(label_2);
+		JLabel label_2 = new JLabel("\u7C7B\u578B");
+		label_2.setBounds(446, 3, 63, 18);
+		searchPanel.add(label_2);
 					
-					searchButton = new JButton("\u68C0\u7D22");
-					searchButton.setBounds(537, 0, 63, 27);
-					searchPanel.add(searchButton);
+		searchButton = new JButton("\u68C0\u7D22");
+		searchButton.setBounds(767, 3, 63, 27);
+		searchPanel.add(searchButton);
 					
-					searchField = new JTextField("输入要查询的信息");
-					searchField.setBounds(0, 0, 126, 27);
-					searchPanel.add(searchField);
-					searchField.setColumns(10);
+		searchField = new JTextField("输入要查询的信息");
+		searchField.setBounds(0, 0, 158, 27);
+		searchPanel.add(searchField);
+		searchField.setColumns(10);
+		
+		searchPanel.setOpaque(false);
+		
+		JPanel playerPanel = new JPanel();
+		playerPanel.setBounds(628, 53, 230, 430);
+		playerPanel.setOpaque(false);
+		playerPanel.setLayout(null);
+		add(playerPanel);
+		
+		playerLabel = new JLabel("");
+		playerLabel.setBounds(0, 0, 230, 245);
+		playerPanel.add(playerLabel);
 					
-					JPanel playerPanel = new JPanel();
-					playerPanel.setBounds(628, 13, 230, 430);
-					playerPanel.setOpaque(false);
-					add(playerPanel);
-					playerPanel.setLayout(null);
+		JLabel lblNewLabel_1 = new JLabel("\u7403\u5458\u59D3\u540D");
+		lblNewLabel_1.setBounds(0, 296, 99, 18);
+		playerPanel.add(lblNewLabel_1);
 					
-					playerLabel = new JLabel("");
-					playerLabel.setBounds(0, 0, 230, 245);
-					playerPanel.add(playerLabel);
+		JLabel label = new JLabel("\u6240\u5C5E\u7403\u961F");
+		label.setBounds(0, 327, 99, 18);
+		playerPanel.add(label);
 					
-					JLabel lblNewLabel_1 = new JLabel("\u7403\u5458\u59D3\u540D");
-					lblNewLabel_1.setBounds(0, 296, 99, 18);
-					playerPanel.add(lblNewLabel_1);
-					
-					JLabel label = new JLabel("\u6240\u5C5E\u7403\u961F");
-					label.setBounds(0, 327, 99, 18);
-					playerPanel.add(label);
-					
-					lblName = new JLabel("Name");
-					lblName.setBounds(117, 296, 99, 18);
-					playerPanel.add(lblName);
-					
-					lblTeam = new JLabel("Team");
-					lblTeam.setBounds(117, 327, 99, 18);
-					playerPanel.add(lblTeam);
-					
-					searchField.addFocusListener(new ClickAdapter());
-					searchButton.addActionListener(new ClickListener());
+		lblName = new JLabel("Name");
+		lblName.setBounds(117, 296, 99, 18);
+		playerPanel.add(lblName);
+		
+		lblTeam = new JLabel("Team");
+		lblTeam.setBounds(117, 327, 99, 18);
+		playerPanel.add(lblTeam);
+		
+		searchField.addFocusListener(new ClickAdapter());
+		searchButton.addActionListener(new ClickListener());
 		boxInitialization();
+		typeBox.setOpaque(false);
+		add(scrollPane);
 		SearchListener s = new SearchListener();
 		s.actionPerformed((ActionEvent)searchButton.getAction());
 		this.table.setRowHeight(30);
-		
 	}
 	
-	public void resizeTable(boolean bool) { 
+	/*public void resizeTable(boolean bool) { 
         Dimension containerwidth = null; 
         if (!bool) { 
             //初始化时，父容器大小为首选大小，实际大小为0 
@@ -234,7 +286,7 @@ public class PlayerTablePanel  extends JPanel implements TableModelListener {
             table. 
               setAutoResizeMode(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS); 
         } 
-}
+}*/
 	public void tableChanged(TableModelEvent e) {
 		// TODO Auto-generated method stub
 		int row = e.getFirstRow();  
