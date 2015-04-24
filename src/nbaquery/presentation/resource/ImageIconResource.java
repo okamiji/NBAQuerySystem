0b15cb43102da9b3c6a602e9f7d86e30f04542b2
Luo Haoran <1910567646@qq.com>
2015-04-24 15:55:42 +0800

    .

---
+++ src/nbaquery/presentation/resource/ImageIconResource.java
@@ -0,0 +1,22 @@
+package nbaquery.presentation.resource;
+
+import java.util.TreeMap;
+
+import javax.swing.ImageIcon;
+
+public final class ImageIconResource
+{
+	private static final TreeMap<String, ImageIcon> image
+		= new TreeMap<String, ImageIcon>();
+	
+	public static ImageIcon getImageIcon(String imageFile)
+	{
+		ImageIcon img;
+		if((img = image.get(imageFile)) == null)
+		{
+			img = new ImageIcon(imageFile);
+			image.put(imageFile, img);
+		}
+		return img;
+	}
+}
