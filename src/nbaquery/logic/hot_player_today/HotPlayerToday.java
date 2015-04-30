d525006ffe354c634b92a8b9a8b9ad0e8165266b
Luo Haoran <1910567646@qq.com>
2015-05-01 02:08:04 +0800

    .

--- src/nbaquery/logic/hot_player_today/HotPlayerToday.java
+++ src/nbaquery/logic/hot_player_today/HotPlayerToday.java
@@ -1,16 +1,10 @@
 package nbaquery.logic.hot_player_today;
 
-import java.util.Collection;
-
-import nbaquery.data.Column;
-import nbaquery.data.Row;
 import nbaquery.data.Table;
 import nbaquery.data.TableHost;
 import nbaquery.data.query.GroupQuery;
 import nbaquery.data.query.NaturalJoinQuery;
-import nbaquery.logic.LogicPipeline;
 import nbaquery.logic.LogicWatcher;
-import nbaquery.logic.NativeTablePipeline;
 import nbaquery.logic.SumColumnInfo;
 
 public class HotPlayerToday {
@@ -26,6 +20,7 @@ public class HotPlayerToday {
 	}
 	
 	public Table getTable() {	
+		
 		if(base.checkDepenency())
 		{
 			GroupQuery groupQuery = new GroupQuery(base.getTable(), new String[]{"player_name"},
@@ -43,6 +38,7 @@ public class HotPlayerToday {
 			tableHost.performQuery(joinQuery, "hot_player_performance_today");
 			table = tableHost.getTable("hot_player_performance_today");
 		}
+		
 		return table;
 	}
 }
--- src/nbaquery/logic/hot_player_today/HotPlayerTodayPerformanceSelect.java
+++ src/nbaquery/logic/hot_player_today/HotPlayerTodayPerformanceSelect.java
@@ -1,46 +1,54 @@
 package nbaquery.logic.hot_player_today;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import nbaquery.data.Column;
 import nbaquery.data.Row;
 import nbaquery.data.Table;
 import nbaquery.data.TableHost;
 import nbaquery.data.query.SelectProjectQuery;
 import nbaquery.logic.LogicPipeline;
 import nbaquery.logic.LogicWatcher;
-import nbaquery.logic.infrustructure.DirectMatchNaturalJoinPerformance;
 import nbaquery.logic.infrustructure.MatchNaturalJoinPerformance;
 
 
 public class HotPlayerTodayPerformanceSelect implements LogicPipeline {
 
 	public TableHost tableHost;
-	protected LogicWatcher base;
+	protected LogicWatcher joinedPerformance;
 	protected Table table;
 	protected String date;
+	protected String season;
 	
-	public HotPlayerTodayPerformanceSelect(TableHost tableHost, MatchNaturalJoinPerformance base){
+	protected Table match;
+	protected Column match_season;
+	protected Column match_date;
+	
+	public HotPlayerTodayPerformanceSelect(TableHost tableHost, MatchNaturalJoinPerformance joinedPerformance){
 		this.tableHost = tableHost;
-		this.base = new LogicWatcher(base);
-	}
-	
-	public String getDate(){
-		Date uDate=new Date();
-		SimpleDateFormat df=new SimpleDateFormat("MM-dd");
-		String result=df.format(uDate);
-		return result;
+		this.joinedPerformance = new LogicWatcher(joinedPerformance);
+		this.match = this.tableHost.getTable("match");
+		this.match_season = this.match.getColumn("match_season");
+		this.match_date = this.match.getColumn("match_date");
 	}
 	
 	@Override
 	public Table getTable() {
-		this.date = getDate();
-		this.date = "02-03";
-		if(base.checkDepenency())
+		boolean hasMatchChanged = match.hasTableChanged(this);
+		if(joinedPerformance.checkDepenency() || hasMatchChanged)
 		{
+			if(hasMatchChanged)
+			{
+				Row[] rows = match.getRows();
+				if(rows.length <= 0) return null;
+				Row lastMatch = rows[rows.length - 1];
+				season = (String) match_season.getAttribute(lastMatch);
+				date = (String) match_date.getAttribute(lastMatch);
+			}
+			
 			try
 			{
-				SelectProjectQuery query = new SelectProjectQuery("match_natural_join_performance.match_date='%1'".replace("%1", date), base.getTable());
+				SelectProjectQuery query = new SelectProjectQuery(
+					"match_natural_join_performance.match_date='%1' and match_natural_join_performance.match_season='%2'"
+						.replace("%1", date).replace("%2", season), joinedPerformance.getTable());
 				tableHost.performQuery(query, "todayPerformance");
 			}
 			catch (Exception e)
