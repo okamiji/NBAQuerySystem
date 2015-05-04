25e418562527dcc3c0d9ed7cac12f48a19ef6cee
Luo Haoran <1910567646@qq.com>
2015-05-04 11:23:26 +0800

    .

--- src/nbaquery/presentation3/match/CompareMatchSubPanel.java
+++ src/nbaquery/presentation3/match/CompareMatchSubPanel.java
@@ -1,8 +1,11 @@
 package nbaquery.presentation3.match;
 
 import java.awt.Graphics;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import nbaquery.data.Table;
 import nbaquery.logic.match.NewMatchService;
@@ -24,13 +27,15 @@ public abstract class CompareMatchSubPanel extends JPanel
 	protected int year;
 	protected int month;
 	
+	protected JTextField fromSeason = new JTextField(), toSeason = new JTextField();
+	
 	public CompareMatchSubPanel(NewMatchService matchService, int width, int height, int componentWidth)
 	{
 		this.matchService = matchService;
 		this.setSize(width, height);
 		this.setLayout(null);
 		
-		matchTableModel = new MatchTableModel((width - componentWidth - 4) / 7 - 1);
+		matchTableModel = new MatchTableModel((width - componentWidth - 4) / 7);
 		
 		displayTable = new DisplayTable(matchTableModel, matchTableModel);
 		displayTable.setBounds(2, 22, width - componentWidth - 4, height - 26);
@@ -52,10 +57,100 @@ public abstract class CompareMatchSubPanel extends JPanel
 		};
 		this.monthSelector.setBounds(width - componentWidth - 6 - 100, 2, 100, 20);
 		this.add(monthSelector);
+		
+		fromSeason.addFocusListener(new FocusListener()
+		{
+			boolean shouldTest = false;
+			@Override
+			public void focusGained(FocusEvent arg0)
+			{
+				shouldTest = true;
+			}
+
+			@Override
+			public void focusLost(FocusEvent arg0)
+			{
+				if(!shouldTest) return;
+				shouldTest = false;
+				try
+				{
+					int fromSeasonValue = Integer.parseInt(fromSeason.getText());
+					if(month < 6) year = fromSeasonValue + 2001;
+					else year = fromSeasonValue + 2000;
+				}
+				catch(Exception e)
+				{
+					
+				}
+				fireTableSwitch();
+			}
+		});
+		
+		
+		toSeason.addFocusListener(new FocusListener()
+		{
+			boolean shouldTest = false;
+			@Override
+			public void focusGained(FocusEvent arg0)
+			{
+				shouldTest = true;
+			}
+
+			@Override
+			public void focusLost(FocusEvent arg0)
+			{
+				if(!shouldTest) return;
+				shouldTest = false;
+				try
+				{
+					int toSeasonValue = Integer.parseInt(toSeason.getText());
+					if(month < 6) year = toSeasonValue + 2000;
+					else year = toSeasonValue + 1999;
+				}
+				catch(Exception e)
+				{
+					
+				}
+				fireTableSwitch();
+			}
+		});
 	}
 	
 	protected void fireTableSwitch()
 	{
+		if(month < 6)
+		{
+			int fromSeasonValue = year - 2001;
+			int toSeasonValue = year - 2000;
+			if(fromSeasonValue > 10)
+				this.fromSeason.setText("" + fromSeasonValue);
+			else this.fromSeason.setText("0" + fromSeasonValue);
+			
+			if(toSeasonValue > 10)
+				this.toSeason.setText("" + toSeasonValue);
+			else this.toSeason.setText("0" + toSeasonValue);
+		}
+		else
+		{
+			int fromSeasonValue = year - 2000;
+			int toSeasonValue = year - 1999;
+			if(fromSeasonValue > 10)
+				this.fromSeason.setText("" + fromSeasonValue);
+			else this.fromSeason.setText("0" + fromSeasonValue);
+			
+			if(toSeasonValue > 10)
+				this.toSeason.setText("" + toSeasonValue);
+			else this.toSeason.setText("0" + toSeasonValue);
+		}
+		try
+		{
+		if(this.monthSelector != null) 
+			this.monthSelector.setText(this.monthSelector.selections[month]);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 		this.matchTableModel.switchToDate(year, month);
 	}
 	
