e3381b955d28b9975475540277a2c8d75f3df237
Luo Haoran <1910567646@qq.com>
2015-05-05 22:54:21 +0800

    .

---
+++ src/nbaquery_test/presentation/DetailedInfoContainerStub.java
@@ -0,0 +1,44 @@
+package nbaquery_test.presentation;
+
+import nbaquery.data.Row;
+import nbaquery.presentation3.DetailedInfoContainer;
+
+public class DetailedInfoContainerStub implements DetailedInfoContainer
+{
+
+	@Override
+	public void displayPlayerInfo(Row player, boolean s)
+	{
+		System.out.println("=================================================================");
+		System.out.println(player.getDeclaredTable().getColumn("player_name").getAttribute(player));
+		System.out.println(player.getDeclaredTable().getColumn("player_age").getAttribute(player));
+		System.out.println(player.getDeclaredTable().getColumn("player_exp").getAttribute(player));
+		System.out.println(player.getDeclaredTable().getColumn("player_birth").getAttribute(player));
+		System.out.println(player.getDeclaredTable().getColumn("player_school").getAttribute(player));
+		System.out.println(player.getDeclaredTable().getColumn("player_height").getAttribute(player));
+		System.out.println(player.getDeclaredTable().getColumn("player_weight").getAttribute(player));
+		System.out.println("=================================================================");
+	}
+
+	@Override
+	public void displayTeamInfo(Row team, boolean s)
+	{
+		System.out.println("=================================================================");
+		System.out.println(team.getDeclaredTable().getColumn("team_name").getAttribute(team));
+		System.out.println(team.getDeclaredTable().getColumn("team_name_abbr").getAttribute(team));
+		System.out.println("=================================================================");
+	}
+
+	@Override
+	public void displayMatchInfo(Row match, boolean stacked)
+	{
+		System.out.println("=================================================================");
+		System.out.println(match.getDeclaredTable().getColumn("match_id").getAttribute(match));
+		System.out.println(match.getDeclaredTable().getColumn("match_season").getAttribute(match));
+		System.out.println(match.getDeclaredTable().getColumn("match_date").getAttribute(match));
+		System.out.println(match.getDeclaredTable().getColumn("match_host_abbr").getAttribute(match));
+		System.out.println(match.getDeclaredTable().getColumn("match_guest_abbr").getAttribute(match));
+		System.out.println("=================================================================");
+	}
+	
+}
