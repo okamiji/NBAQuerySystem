2b0a0af989b55982785b3fced03a96fba0571e7f
aegistudio <1910567646@qq.com>
2015-06-04 03:28:45 +0000

    .

--- src/nbaqueryBusinessLogicService/PlayerService.java
+++
@@ -1,7 +0,0 @@
-package nbaqueryBusinessLogicService;
-
-public interface PlayerService {
-
-	public String[][] searchForPlayers(boolean type,String head,boolean upOrDown,String position,String league);
-	
-}
--- src/nbaqueryBusinessLogicService/TeamService.java
+++
@@ -1,7 +0,0 @@
-package nbaqueryBusinessLogicService;
-
-public interface TeamService {
-	
-	public String[][] searchForTeams(boolean type,String head,boolean upOrDown);
-
-}
