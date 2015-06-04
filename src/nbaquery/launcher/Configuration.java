bd54db0f67fb8f386aed323dd4dee71ec2514108
aegistudio <1910567646@qq.com>
2015-06-04 00:17:24 +0000

    Update.

--- src/nbaquery/launcher/Configuration.java
+++ src/nbaquery/launcher/Configuration.java
@@ -83,6 +83,161 @@ public class Configuration
 	protected void setupLogicConfig(Document dom, Element logicConfig)
 	{
 		logicConfig.setAttribute("installer", "nbaquery.logic.LogicInstaller");
+		
+		
+		logicConfig.appendChild(dom.createComment("infrustructure"));
+		
+		logicConfig.appendChild(makePipelineNode(dom, 
+				"match_natural_join_performance", 
+				"nbaquery.logic.infrustructure.DirectMatchNaturalJoinPerformance",
+				new String[]{})
+		);
+		logicConfig.appendChild(makePipelineNode(dom,
+				"match_team_performance",
+				"nbaquery.logic.infrustruture.MatchTeamPerformance",
+				new String[]{"match_natural_join_performance"})
+		);
+		logicConfig.appendChild(makePipelineNode(dom,
+				"rival_team_performance",
+				"nbaquery.logic.infrustructure.RivalTeamPerformance",
+				new String[]{"match_team_performance"})
+		);
+		logicConfig.appendChild(makePipelineNode(dom,
+				"rival_team_natural_join",
+				"nbaquery.logic.infrustructure.RivalTeamNaturalJoin",
+				new String[]{"rival_team_performance", "match_team_performance"})
+		);
+		logicConfig.appendChild(makePipelineNode(dom,
+				"player_performance",
+				"nbaquery.logic.infrustructure.PlayerPerformance",
+				new String[]{"match_natural_join_performance", "match_team_performance", "rival_team_performance"})
+		);
+		
+		
+		logicConfig.appendChild(dom.createComment("gross_team"));
+		
+		logicConfig.appendChild(makePipelineNode(dom,
+				"gross_team_performance",
+				"nbaquery.logic.gross_team.GrossTeamPerformance",
+				new String[]{"match_team_performance"})
+		);
+		logicConfig.appendChild(makePipelineNode(dom,
+				"gross_rival_performance",
+				"nbaquery.logic.gross_team.GrossRivalPerformance",
+				new String[]{"rival_team_performance"})
+		);
+		logicConfig.appendChild(makePipelineNode(dom,
+				"gross_team_natural_join",
+				"nbaquery.logic.gross_team.GrossTeamNaturalJoin",
+				new String[]{"gross_rival_performance", "gross_team_performance"})
+		);
+		logicConfig.appendChild(makePipelineNode(dom,
+				"gross_team",
+				"nbaquery.logic.gross_team.GrossTeam",
+				new String[]{"gross_team_natural_join"})
+		);
+		
+		
+		logicConfig.appendChild(dom.createComment("gross_player"));
+		
+		logicConfig.appendChild(makePipelineNode(dom,
+				"gross_player_performance",
+				"nbaquery.logic.gross_player.GrossPlayerPerformance",
+				new String[]{"player_performance"})
+		);
+		logicConfig.appendChild(makePipelineNode(dom,
+				"gross_player",
+				"nbaquery.logic.gross_player.GrossPlayer",
+				new String[]{"gross_player_performance"})
+		);
+		
+		
+		logicConfig.appendChild(dom.createComment("average_team"));
+		
+		logicConfig.appendChild(makePipelineNode(dom,
+				"derived_team_performance",
+				"nbaquery.logic.average_team.DerivedTeamPerformance",
+				new String[]{"rival_team_natural_join"})
+		);
+		logicConfig.appendChild(makePipelineNode(dom,
+				"average_team",
+				"nbaquery.logic.average_team.AverageTeam",
+				new String[]{"derived_team_performance"})
+		);
+
+		
+		logicConfig.appendChild(dom.createComment("average_player"));
+		
+		logicConfig.appendChild(makePipelineNode(dom,
+				"derive_player_performance",
+				"nbaquery.logic.average_player.DerivedPlayerPerformance",
+				new String[]{"player_performance"})
+		);
+		logicConfig.appendChild(makePipelineNode(dom,
+				"average_player",
+				"nbaquery.logic.average_player.AveragePlayer",
+				new String[]{"derive_player_performance"})
+		);
+		
+		
+		logicConfig.appendChild(dom.createComment("hot_player"));
+		
+		logicConfig.appendChild(makePipelineNode(dom,
+				"hot_player_today_p_s",
+				"nbaquery.logic.hot_player_today.HotPlayerTodayPerformanceSelect",
+				new String[]{"match_natural_join_performance"})
+		);
+		logicConfig.appendChild(makePipelineNode(dom,
+				"hot_player_today",
+				"nbaquery.logic.hot_player_today.HotPlayerToday",
+				new String[]{"hot_player_today_p_s"})
+		);
+		
+		
+		logicConfig.appendChild(dom.createComment("progress_player"));
+		
+		logicConfig.appendChild(makePipelineNode(dom,
+				"progress_player_group",
+				"nbaquery.logic.progress_player.ProgressPlayerGroup",
+				new String[]{"match_natural_join_performance"})
+		);
+		logicConfig.appendChild(makePipelineNode(dom,
+				"progress_player",
+				"nbaquery.logic.progress_player.ProgressPlayer",
+				new String[]{"progress_player_group"})
+		);
+		
+		
+		logicConfig.appendChild(dom.createComment("hot_team"));
+		
+		logicConfig.appendChild(makePipelineNode(dom,
+				"hot_team_select",
+				"nbaquery.logic.hot_team_today.HotTeamTodayPerformanceSelect",
+				new String[]{"match_natural_join_performance"})
+		);
+		logicConfig.appendChild(makePipelineNode(dom,
+				"hot_team_today",
+				"nbaquery.logic.hot_team_today.HotTeamToday",
+				new String[]{"hot_team_select"})
+		);
+	}
+	
+	protected Node makePipelineNode(Document dom, String pipelineName, String pipelineClazz, String[] dependencies)
+	{
+		Element element = dom.createElement("pipeline");
+		element.setAttribute("name", pipelineName);
+		element.setAttribute("class", pipelineClazz);
+		
+		if(dependencies != null && dependencies.length > 0)
+		{
+			for(String dependency : dependencies)
+			{
+				Element dependencyNode = dom.createElement("dependency");
+				dependencyNode.setTextContent(dependency);
+				element.appendChild(dependencyNode);
+			}
+		}
+		return element;
 	}
 	
 	protected void setupDataConfig(Document dom, Element dataConfig)
@@ -128,4 +283,5 @@ public class Configuration
 	{
 		new Configuration(new File("config.xml"));
 	}
+	
 }
