49b5c3d3d0937b9f4666dff6629797d4a8ab8a31
okamiji <okamiji@qq.com>
2015-06-17 14:30:15 +0800

    .

---
+++ src/nbaquery/statistics/RunPython.java
@@ -0,0 +1,42 @@
+package nbaquery.statistics;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class RunPython {
+	public static void run_python(String file_path){
+		try {
+			Process pr = Runtime.getRuntime().exec("python " + file_path);  
+			BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				System.out.println(line);
+			}
+			in.close();
+			pr.waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void run_python(String file_path, String... argv){
+		try {
+			String command = "python " + file_path;
+			for(String s : argv){
+				command += " " + s;
+			}
+			System.out.println(command);
+			
+			Process pr = Runtime.getRuntime().exec(command);  
+			BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				System.out.println(line);
+			}
+			in.close();
+			pr.waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
