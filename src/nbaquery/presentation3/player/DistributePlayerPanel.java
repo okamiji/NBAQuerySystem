bed04c102e5c46a59eefc7b79ad0c50a23612ba3
aegistudio <1910567646@qq.com>
2015-06-17 11:20:08 +0800

    .

--- src/nbaquery/presentation3/player/DistributePlayerPanel.java
+++ src/nbaquery/presentation3/player/DistributePlayerPanel.java
@@ -45,63 +45,7 @@ public class DistributePlayerPanel extends JPanel
 		playerTable.setRowHeight(playerTable.getHeight() / (sectionPerPageParam + 1));
 		basicData = new ComparePlayerModel()
 		{
-			{
-				setSectionPerPage(sectionPerPageParam);
-				DefaultTableColumn season = columnModel.addColumn("\u8D5B\u5B63", "match_season");
-				season.padding = 10;	keywordMap.put(season, new String[]{"match_season"});
-				
-				DefaultTableColumn portrait = columnModel.addColumn("", "player_portrait");
-				portrait.padding = playerTable.getHeight() / (sectionPerPage + 1);
-				
-				DefaultTableColumn player_name = columnModel.addColumn("\u7403\u5458\u540D\u79F0", "player_name");
-				player_name.padding = 70;	keywordMap.put(season, new String[]{"player_name"});
-				
-				DefaultTableColumn logo = columnModel.addColumn("", "team_logo");
-				logo.padding = playerTable.getHeight() / (sectionPerPage + 1);
-				
-				DefaultTableColumn team_name = columnModel.addColumn("\u7403\u961F", "team_name_abbr");
-				team_name.padding = 5;	keywordMap.put(team_name, new String[]{"team_name_abbr"});
-				
-				DualTableColumn attendance = new DualTableColumn("\u573A\u6B21", "first_count", "game_count", "%1-%2"); attendance.padding = 3;
-				columnModel.addColumn(attendance);	keywordMap.put(attendance, new String[]{"game_count", "first_count"});
-				
-				GameTimeColumn game_time = new GameTimeColumn();
-				columnModel.addColumn(game_time);	keywordMap.put(game_time, new String[]{"game_time"});
-				
-				DualTableColumn foul_shoot = new DualTableColumn("\u7F5A\u7403", "foul_shoot_score", "foul_shoot_count", "%1-%2"); foul_shoot.padding = 5;
-				columnModel.addColumn(foul_shoot);	keywordMap.put(foul_shoot, new String[]{"foul_shoot_count", "foul_shoot_score"});
-				
-				DualTableColumn two_shoot = new DualTableColumn("\u6295\u7BEE", "shoot_score", "shoot_count", "%1-%2"); two_shoot.padding = 5;
-				columnModel.addColumn(two_shoot);	keywordMap.put(two_shoot, new String[]{"shoot_count", "shoot_score"});
-				
-				DualTableColumn three_shoot = new DualTableColumn("\u4E09\u5206", "three_shoot_score", "three_shoot_count", "%1-%2"); three_shoot.padding = 5;
-				columnModel.addColumn(three_shoot);	keywordMap.put(three_shoot, new String[]{"three_shoot_count", "three_shoot_score"});
-				
-				DefaultTableColumn self_score = columnModel.addColumn("\u5F97\u5206", "self_score");
-				self_score.padding = 5;			keywordMap.put(self_score, new String[]{"self_score"});
-				
-				DualTableColumn attack_defence = new DualTableColumn("\u653B\u5B88", "attack_board", "defence_board", "%1-%2"); attack_defence.padding = 5;
-				columnModel.addColumn(attack_defence);	keywordMap.put(attack_defence, new String[]{"attack_board", "defence_board"});
-				
-				DefaultTableColumn total_board = columnModel.addColumn("\u7BEE\u677F", "total_board");
-				total_board.padding = 6;	keywordMap.put(total_board, new String[]{"total_board"});
-				
-				DefaultTableColumn assist = columnModel.addColumn("\u52A9\u653B", "assist");
-				assist.padding = 6;		keywordMap.put(assist, new String[]{"assist"});
-				
-				DefaultTableColumn cap = columnModel.addColumn("\u76D6\u5E3D", "cap");
-				cap.padding = 6;	keywordMap.put(cap, new String[]{"cap"});
-				
-				DefaultTableColumn steal = columnModel.addColumn("\u62A2\u65AD", "steal");
-				steal.padding = 6;	keywordMap.put(steal, new String[]{"steal"});
-				
-				DefaultTableColumn miss = columnModel.addColumn("\u5931\u8BEF", "miss");
-				miss.padding = 6;	keywordMap.put(miss, new String[]{"miss"});
-				
-				DefaultTableColumn foul = columnModel.addColumn("\u72AF\u89C4", "foul");
-				foul.padding = 6;	keywordMap.put(foul, new String[]{"foul"});
-			}
-		};
+
 		
 		analyticData = new ComparePlayerModel()
 		{
@@ -176,8 +120,10 @@ public class DistributePlayerPanel extends JPanel
 		this.add(dataScope);
 		
 		
-		final String[] keywordName = new String[]{"self_score", "total_board", "assist", "cap", "steal", "miss", "foul"};
-		String[] keywordLabel = new String[]{"\u5F97\u5206", "\u7BEE\u677F", "\u52A9\u653B", "\u76D6\u5E3D", "\u62A2\u65AD", "\u5931\u8BEF", "\u72AF\u89C4"};
+		final String[] keywordName = new String[]{"self_score", "total_board", "assist", "cap", "steal", "miss", "foul", "efficiency", "gmsc_efficiency",
+				"foul_shoot_rate", "shoot_rate", "three_shoot_rate", "true_shoot_rate", "shoot_efficiency", "assist_rate", "steal_rate", "cap_rate", "miss_rate", "usage_rate"};
+		String[] keywordLabel = new String[]{"\u5F97\u5206", "\u7BEE\u677F", "\u52A9\u653B", "\u76D6\u5E3D", "\u62A2\u65AD", "\u5931\u8BEF", "\u72AF\u89C4", "\u6548\u7387", "GmSc",
+				"\u7F5A\u7403\u547D\u4E2D", "\u4E8C\u5206\u547D\u4E2D", "\u4E09\u5206\u547D\u4E2D", "\u771F\u5B9E\u547D\u4E2D", "\u6295\u7BEE\u6548\u7387", "\u52A9\u653B\u6548\u7387", "\u62A2\u65AD\u6548\u7387", "\u76D6\u5E3D\u6548\u7387", "\u5931\u8BEF\u7387", "\u4F7F\u7528\u7387"};
 		
 		final DropList dataModel = new DropList(keywordLabel)
 		{
